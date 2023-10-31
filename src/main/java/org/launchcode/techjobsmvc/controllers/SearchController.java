ed30aba53405e77f0fad29a9adb1265e3afeed6b
Sarah Metoxen <slmetoxen.com>
2023-10-31 12:35:37 -0500

    Fourth search test case not working on task #4.

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,12 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
 import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -24,9 +27,21 @@ public class SearchController {
 //    // TODO #3 - Create a handler to process a search request and render the updated search view.
     @PostMapping("results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
-        model.addAttribute("searchType", searchType);
-        model.addAttribute("searchTerm", searchTerm);
-        return "/search/results";
+
+        ArrayList<Job> jobs;
+
+        if (searchType.equals("all") || searchTerm.isEmpty()) {
+            jobs = JobData.findAll();
+
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+
+
+        }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+        return "list-jobs";
+        }
     }
-}
+
 
